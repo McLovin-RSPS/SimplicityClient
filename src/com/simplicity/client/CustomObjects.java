2846c13310fb304bf933c1cc46e8afd18081dee6
gabriel boos <gabriel.boos@gmail.com>
2018-11-04 16:25:24 -0300

    Well spawn at home

--- src/com/simplicity/client/CustomObjects.java
+++ src/com/simplicity/client/CustomObjects.java
@@ -21,6 +21,10 @@ public class CustomObjects {
 	}
 	
 	public static final int[][] CUSTOM_OBJECTS = {
+
+			//Well for upgradable at home
+			{3485, 3077, 3500, 0, 0},
+
 			 //Treasure island chests
 			  { 18804, 3039, 2912, 0, 2 },
 
