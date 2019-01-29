6b86a202f3a9f0b548bc50554e632f476f54960e
Blaketon <insane_ownz@hotmail.com>
2019-01-29 13:20:37 +0200

    Sky blue Vitur

--- src/com/simplicity/client/cache/definitions/ItemDefinition.java
+++ src/com/simplicity/client/cache/definitions/ItemDefinition.java
@@ -851,6 +851,21 @@ public final class ItemDefinition {
         	itemDef.editedModelColor = def2.editedModelColor;
         	itemDef.newModelColor = def2.newModelColor;
             break;
+        case 21010:
+            itemDef.modelID = 35742;
+            itemDef.actions = new String[5];
+            itemDef.actions[1] = "Wield";
+            itemDef.name = "Sky blue Vitur";
+            itemDef.description = "It is the Sky blue Vitur.";
+            itemDef.modelZoom = 2200;
+            itemDef.stackable = false;
+            itemDef.rotationX = 23;
+            itemDef.rotationY = 327;
+            itemDef.maleEquip1 = 35371;
+            itemDef.femaleEquip1 = 35371;
+            itemDef.editedModelColor = new int[] { 784, 790, 796, 536, 61, 78, 49 };
+            itemDef.newModelColor = new int[] { 689484, 689484, 689484, 689484, 689484, 689484, 689484 };
+            break;
         	case 18339:
         		itemDef.name = "Gold bag";
         		itemDef.actions[0] = "Check";
