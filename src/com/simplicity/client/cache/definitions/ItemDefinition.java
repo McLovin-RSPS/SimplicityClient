1e7ebb1bc42f77be57395155a972778f7b387fa7
adoraps <intdubpk@gmail.com>
2019-10-27 17:08:29 +0100

    Vitur customized edit
    Vitur customized edit

--- src/com/simplicity/client/cache/definitions/ItemDefinition.java
+++ src/com/simplicity/client/cache/definitions/ItemDefinition.java
@@ -1401,7 +1401,7 @@ public final class ItemDefinition {
             itemDef.maleEquip1 = 35371;
             itemDef.femaleEquip1 = 35371;
             itemDef.editedModelColor = new int[] { 784, 790, 796, 536, 61, 78, 49 };
-            itemDef.newModelColor = new int[] { 1024, 1024, 1024, 1024, 54220, 54220, 54220 };
+            itemDef.newModelColor = new int[] { 1024, 1024, 1024, 90, 54220, 54220, 54220 };
             break;
         case 21016: // Relax's custom Vitur XI
             itemDef.modelID = 35742;
