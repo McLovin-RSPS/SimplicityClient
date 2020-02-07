082008814f056ad307a59b8211e4367906a0be29
19201 <40706508+19201@users.noreply.github.com>
2020-02-08 06:54:18 +0700

    Red Dragonstone

--- src/com/simplicity/client/cache/definitions/custom/CustomRecolor.java
+++ src/com/simplicity/client/cache/definitions/custom/CustomRecolor.java
@@ -22,11 +22,11 @@ public enum CustomRecolor {
 	GROUP_IRONMAN_LEGS(56007, 42812, "Group ironman legs", new int[] { 24, 37, 49, 53, 57, 70, 78, 86, 90, 94, 99, 103, 107, 111, 115  }, new int[] { 16026, 16026, 16026, 16026, 16026, 16026, 16026, 16023, 16026, 16026, 16026, 16026, 16022, 16026, 16026 }),
 	
 	
-	RED_DRAGONSTONE_HELM(56005, 54034, "Dragonstone full helm", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15 }),
-	RED_DRAGONSTONE_PLATE(56006, 54037, "Dragonstone platebody", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15 }),
-	RED_DRAGONSTONE_LEGS(56007, 54040, "Dragonstone platelegs", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15 }),
-	RED_DRAGONSTONE_BOOTS(56008, 54043, "Dragonstone boots", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15 }),
-	RED_DRAGONSTONE_GLOVES(56009, 54046, "Dragonstone gloves", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15 }),
+	RED_DRAGONSTONE_HELM(56008, 54034, "Dragonstone full helm", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133, 52122 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15, 0 }),
+	RED_DRAGONSTONE_PLATE(56009, 54037, "Dragonstone platebody", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133, 52122 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15, 0 }),
+	RED_DRAGONSTONE_LEGS(56010, 54040, "Dragonstone platelegs", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133, 52122 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15, 0 }),
+	RED_DRAGONSTONE_BOOTS(56011, 54043, "Dragonstone boots", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133, 52122 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15, 0 }),
+	RED_DRAGONSTONE_GLOVES(56012, 54046, "Dragonstone gloves", new int[] { 36252, 36257, 36133, 37165, 43059, 43067, 51111, 51133, 52122 }, new int[] { 933, 933, 933, 933, 933, 933, 0, 15, 0 }),
 	;
 	
 	/**
