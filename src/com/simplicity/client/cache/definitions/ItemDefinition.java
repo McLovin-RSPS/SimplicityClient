a67e18ae60a605d67b3ded96e614d0d96a341b5d
adoraps <intdubpk@gmail.com>
2020-03-10 16:08:26 +0100

    Custom order: SOV DS White
    Custom order: SOV DS White

--- src/com/simplicity/client/cache/definitions/ItemDefinition.java
+++ src/com/simplicity/client/cache/definitions/ItemDefinition.java
@@ -1703,6 +1703,22 @@ public final class ItemDefinition {
             itemDef.newModelColor = new int[] { 56220, 56220, 56220, 56220, 685484, 685484, 685484 };
             break;
             
+        case 22010: // Scythe of Vitur HELLRATY
+            itemDef.modelID = 35742;
+            itemDef.actions = new String[5];
+            itemDef.actions[1] = "Wield";
+            itemDef.name = "@red@Scythe of Vitur DS";
+            itemDef.description = "It is the Scythe of Vitur Dragonstone (white).";
+            itemDef.modelZoom = 2200;
+            itemDef.stackable = false;
+            itemDef.rotationX = 23;
+            itemDef.rotationY = 327;
+            itemDef.maleEquip1 = 35371;
+            itemDef.femaleEquip1 = 35371;
+            itemDef.editedModelColor = new int[] { 784, 790, 796, 536, 61, 78, 49 };
+            itemDef.newModelColor = new int[] { 90, 90, 90, 90, 90, 90, 90 };
+            break;
+            
       /*  case 21079: // Water blue No custom order
             itemDef.modelID = 35742;
             itemDef.actions = new String[5];
