9831ebd9bc6c71cabb6875fac65151b4dfb6c013
Blaketon <insane_ownz@hotmail.com>
2020-03-14 10:56:05 +0200

    Small tweak for anims

--- src/com/simplicity/client/Client.java
+++ src/com/simplicity/client/Client.java
@@ -14090,7 +14090,12 @@ public class Client extends RSApplet {
 	            }
 	            entity.nextAnimationFrame = entity.currentAnimFrame + 1;
 	            if (entity.nextAnimationFrame >= animation_3.frameCount) {
-					entity.nextAnimationFrame = getNextFrame(entity);
+	            	if (entity.anInt1530 >= animation_3.frameCount) {
+	                    entity.nextAnimationFrame = getNextFrame(entity);
+	                }
+	                if (entity.nextAnimationFrame < 0 || entity.nextAnimationFrame >= animation_3.frameCount) {
+	                    entity.nextAnimationFrame = getNextFrame(entity);
+	                }
 				}
 	            
 	            entity.aBoolean1541 = animation_3.oneSquareAnimation;
