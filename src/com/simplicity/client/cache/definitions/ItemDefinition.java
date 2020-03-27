0431460c10ebb8b93a771db0c8dba7f8910f47c5
adoraps <intdubpk@gmail.com>
2020-03-27 03:01:01 +0100

    Necklace model priorties & objects to block ecto wilderness zone
    Necklace model priorties & objects to ecto farming patch

--- src/com/simplicity/client/cache/definitions/ItemDefinition.java
+++ src/com/simplicity/client/cache/definitions/ItemDefinition.java
@@ -505,6 +505,14 @@ public final class ItemDefinition {
         //Necklace of anguish (or)
         priorityModels.add(12778);
         priorityModels.add(12817);
+        
+        //Necklaces
+        priorityModels.add(31524);
+        priorityModels.add(31227);
+        priorityModels.add(31233);
+        priorityModels.add(31510);
+        priorityModels.add(31228);
+        priorityModels.add(31232);
 
         priorityModels.add(36324); //devout boots
         priorityModels.add(36333); //devout boots
@@ -706,6 +714,18 @@ public final class ItemDefinition {
 	            	itemDef.maleYOffset -= 10;
 	            	itemDef.femaleZOffset += 4;
 	            	itemDef.femaleYOffset -= 6;
+	            	break;
+	            	
+	            // OSRS necklaces
+	            case 49553:
+	            case 49547:
+	            case 52249:
+	            case 49535:
+	            case 1704:
+	            	itemDef.maleZOffset += 4;
+	            	itemDef.maleYOffset -= 10;
+	            	itemDef.femaleYOffset -= 10;
+	            	itemDef.modelOffsetY = 16;
 	            	break;
             	
             	// Team cape x and i
