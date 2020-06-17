326fe135cab5c669fde66d6cd27781a01e079fda
Blaketon <insane_ownz@hotmail.com>
2020-06-17 17:52:09 +0300

    Same for obj removal..
    Removed the hardcoded BS.

--- src/com/simplicity/client/Client.java
+++ src/com/simplicity/client/Client.java
@@ -18542,12 +18542,12 @@ public class Client extends RSApplet {
                 return;
             }
             int i11 = anIntArray1177[k5];
-            int height = stream.readUnsignedByte() % 4;
-            int k15 = bigRegionX + (height >> 4 & 7);
-            int l16 = bigRegionY + (height & 7);
+            int positionOffset = stream.readUnsignedByte();
+            int k15 = bigRegionX + (positionOffset >> 4 & 7);
+            int l16 = bigRegionY + (positionOffset & 7);
 
             if (k15 >= 0 && l16 >= 0 && k15 < 104 && l16 < 104) {
-                createObjectSpawnRequest(-1, -1, j8, i11, l16, k5, height, k15, 0);
+                createObjectSpawnRequest(-1, -1, j8, i11, l16, k5, plane, k15, 0);
             }
             return;
         }
