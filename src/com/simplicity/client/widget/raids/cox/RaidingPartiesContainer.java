79d18a0514ddd4ff671ecac02297168c21c01d98
Blaketon <insane_ownz@hotmail.com>
2020-09-01 18:48:29 +0300

    COX Interfaces

---
+++ src/com/simplicity/client/widget/raids/cox/RaidingPartiesContainer.java
@@ -0,0 +1,144 @@
+package com.simplicity.client.widget.raids.cox;
+
+import static com.simplicity.client.RSInterface.interfaceCache;
+import static com.simplicity.client.widget.raids.tob.TheatrePerformersWidget.CENTER_STRING_ID;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.simplicity.client.RSInterface;
+import com.simplicity.client.widget.CustomWidget;
+import com.simplicity.client.widget.listener.WidgetStringListener;
+
+/**
+ * A class that handles the container for the {@link TheatrePerformersWidget}.
+ * 
+ * @author Blake
+ *
+ */
+public class RaidingPartiesContainer extends CustomWidget implements WidgetStringListener {
+	
+	/**
+	 * The widget id.
+	 */
+	public static final int WIDGET_ID = 78_400;
+	
+	/**
+	 * The rectangle height.
+	 */
+	private static final int RECT_HEIGHT = 18;
+	
+	/**
+	 * The width of the container.
+	 */
+	private static final int WIDTH = 424;
+	
+	/**
+	 * The height of the container.
+	 */
+	private static final int HEIGHT = 227;
+	
+	/**
+	 * The start id.
+	 */
+	private static int START_ID;
+	
+	/**
+	 * The ids of the names.
+	 */
+	public static final Set<Integer> ids = new HashSet<>();
+	
+	/**
+	 * Constructs a new {@link RaidingPartiesContainer}.
+	 */
+	public RaidingPartiesContainer() {
+		super(WIDGET_ID);
+	}
+
+	@Override
+	public String getName() {
+		return "Performers container widget";
+	}
+	
+	/**
+	 * The size of the container.
+	 */
+	public static final int ROWS = 20;
+	
+	@Override
+	public void init() {
+		addStringListener(this);
+		getInterface().width = WIDTH;
+		getInterface().height = HEIGHT;
+		getInterface().scrollMax = Math.max(HEIGHT + 1, ROWS * (RECT_HEIGHT + 1));
+		
+		int x = 0;
+		int y = 0;
+		
+		START_ID = id;
+		
+		for (int i = 0; i < ROWS; i++) {
+			RSInterface rect = addRectangleClickable(255, 0, true, WIDTH - 1, RECT_HEIGHT, new String[] { "View party: @lre@Blake" });
+			rect.hovers = true;
+    		rect.hoverType = id - 1;
+    		rect.enabledOpacity = 220;
+    		rect.enabledMouseOverColor = 0xffffff;
+    		rect.hidden = true;
+    		add(rect, x, y);
+    		RSInterface members = addCenteredText("1", 1, 0xff981f);
+    		members.useNewFonts = true;
+    		add(members, 18, 1 + y);
+    		RSInterface targetSize = addCenteredText("/1", 1, 0x9f9f9f);
+    		targetSize.useNewFonts = true;
+    		add(targetSize, 18 + 42, 1 + y);
+    		ids.add(id);
+    		add(addCenteredText("Name", 1, 0xffffff), 18 + 137, 1 + y);
+    		
+    		RSInterface preferredLevel = addCenteredText("126", 1, 0x9f9f9f);
+    		preferredLevel.useNewFonts = true;
+    		add(preferredLevel, 18 + 138 + 94, 1 + y);
+    		
+    		add(addSprite(1453), 18 + 138 + 94 + 41 + 26, 1 + y);
+    		
+    		RSInterface preferredTotalLevel = addCenteredText("-", 1, 0x9f9f9f);
+    		preferredTotalLevel.useNewFonts = true;
+    		add(preferredTotalLevel, 18 + 138 + 94 + 41, 1 + y);
+    		
+    		add(addTimer(25, 14, 0, 0x9f9f9f, "0:00"), 18 + 139 + 94 + 77 + 53, 3 + y);
+			y += RECT_HEIGHT + 1;
+		}
+	}
+	
+	@Override
+	public void onStringUpdate(int id, String string) {
+		if (!ids.contains(id)) {
+			return;
+		}
+		
+		RSInterface rect = interfaceCache[id - 3];
+		
+		if (!string.isEmpty()) {
+			rect.actions = new String[] { "View party: @lre@" + string };
+		}
+		
+		rect.hidden = string.isEmpty();
+		
+		if (rect.hidden) {
+			boolean visible = false;
+			
+			for (int i : ids) {
+				if (!interfaceCache[i - 3].hidden) {
+					visible = true;
+					break;
+				}
+			}
+			
+			interfaceCache[CENTER_STRING_ID].hidden = visible;
+			
+			if (!visible) {
+				getInterface().scrollMax = HEIGHT + 1;
+			}
+		}
+	}
+	
+}
---
+++ src/com/simplicity/client/widget/raids/cox/RaidingPartiesWidget.java
@@ -0,0 +1,117 @@
+package com.simplicity.client.widget.raids.cox;
+
+import com.simplicity.client.RSInterface;
+import com.simplicity.client.widget.CustomWidget;
+import com.simplicity.client.widget.listener.WidgetButtonListener;
+
+/**
+ * A class that handles the Raiding parties widget.
+ * 
+ * @author Blake
+ *
+ */
+public class RaidingPartiesWidget extends CustomWidget implements WidgetButtonListener {
+	
+	/**
+	 * The widget id.
+	 */
+	public static final int WIDGET_ID = 78_300;
+	
+	/**
+	 * The refresh button id.
+	 */
+	public static int REFRESH_BUTTON_ID;
+	
+	/**
+	 * The make party button id.
+	 */
+	public static int MAKE_PARTY_BUTTON_ID;
+	
+	/**
+	 * The sort button start id.
+	 */
+	public static int SORT_BUTTON_START;
+	
+	/**
+	 * The center string id.
+	 */
+	public static int CENTER_STRING_ID;
+	
+	/**
+	 * Constructs a new {@link RaidingPartiesWidget}.
+	 */
+	public RaidingPartiesWidget() {
+		super(WIDGET_ID);
+	}
+
+	@Override
+	public String getName() {
+		return "Raiding Parties";
+	}
+
+	@Override
+	public void init() {
+		addButtonListener(this);
+		
+		int xoff = 6;
+		int yoff = 4;
+		add(addClosableWindow(464, 320, false, getName()), 25, 6);
+		
+		REFRESH_BUTTON_ID = id + 1;
+		add(addDynamicButton("Refresh", 1, 0xff981f, 100, 25), 119, 291);
+		RSInterface makeParty = addDynamicButton("Make Party", 1, 0xff981f, 100, 25);
+		
+		MAKE_PARTY_BUTTON_ID = makeParty.id + 1;
+		
+		add(makeParty, 303, 291);
+		add(addBox(443, 14, 2, 4671301, 0, 250), 41 - xoff, 49 - yoff);
+		add(addBox(443, 243, 2, 4671301, 0, 250), 41 - xoff, 49 - yoff);
+		addWidget(RaidingPartiesContainer.WIDGET_ID, xoff + 31, 58);
+		
+		CENTER_STRING_ID = id;
+		add(addCenteredText("No parties are currently listed.", 1, 16750623), 250, 160);
+		
+		SORT_BUTTON_START = id;
+		add(addSortButton(35, 9, true, "Sort by members"), 43 - xoff, 51 - yoff);
+		add(addSortButton(45, 9, false, "Sort by target size"), 43 - xoff + 36, 51 - yoff);
+		add(addSortButton(145, 9, false, "Sort by leader name"), 43 - xoff + 82, 51 - yoff);
+		add(addSortButton(40, 9, false, "Sort by preferred combat level"), 43 - xoff + 228, 51 - yoff);
+		add(addSortButton(45, 9, false, "Sort by preferred skill total"), 43 - xoff + 269, 51 - yoff);
+		add(addSortButton(20, 9, false, "Sort by challenge mode"), 43 - xoff + 269 + 45, 51 - yoff);
+		add(addSortButton(94, 9, false, "Sort by age"), 43 - xoff + 269 + 45 + 20, 51 - yoff);
+	}
+	
+	public RSInterface addSortButton(int width, int height, boolean isActive, String tooltipBox) {
+		RSInterface button = addSelectableToggleButton("", 1417, 1416, width, height, true, isActive, isActive);
+		
+		button.hovers = true;
+		button.filled = true;
+		button.hoverType = id - 1;
+		button.hoverOpacity = 100;
+		button.defaultOpacity = 150;
+		button.selectedOpacity = 256;
+		button.tooltipBox = tooltipBox;
+		
+		int[] selectable = new int[6];
+		
+		for (int i = SORT_BUTTON_START, ptr = 0; i <= SORT_BUTTON_START + 6; i++) {
+			if (i != id - 1) {
+				selectable[ptr++] = i;
+			}
+		}
+		
+		button.selectableInterfaces = selectable;
+		return button;
+	}
+	
+	@Override
+	public boolean onClick(int id) {
+		if (id >= SORT_BUTTON_START && id <= SORT_BUTTON_START + 6) {
+			RSInterface.setSelectedInterface(id, true);
+			return true;
+		}
+		
+		return false;
+	}
+
+}
