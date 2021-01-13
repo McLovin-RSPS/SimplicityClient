3ece5090fe5499caab9d13ef8330c78c9599937a
Blaketon <insane_ownz@hotmail.com>
2021-01-13 11:02:15 +0200

    Added realism mode to starter

--- src/com/simplicity/client/widget/StarterWidget.java
+++ src/com/simplicity/client/widget/StarterWidget.java
@@ -26,7 +26,7 @@ public class StarterWidget extends RSInterface {
 
 		RSInterface rsi = addInterface(id++);
 
-		rsi.totalChildren(41);
+		rsi.totalChildren(45);
 		
 		int child = 0;
 		
@@ -50,64 +50,70 @@ public class StarterWidget extends RSInterface {
 		rsi.child(child++, id, 140 + 135 + 18, 219);
 		id+=2;
 		
-		addText(id, "Welcome to Simplicity!", tda, 4, 0xff9800, true, true);
-		rsi.child(child++, id, 252, 20);
+		addText(id, "Welcome to Simplicity!", tda, 2, 0xff9800, true, true);
+		rsi.child(child++, id, 255, 30);
 		id++;
 		
 		addRectangle(id, 150, 0, true, 127, 154);
 		rsi.child(child++, id, 13, 84 - 25);
 		id++;
 		
-		addText(id, "- Game Mode -", tda, 2, 0x00a81d, true, true);
+		/*addText(id, "Game Mode", tda, 2, 0xffffff, true, true);
 		rsi.child(child++, id, 75, 64);
-		id++;
+		id++;*/
 		
-		String[] text = new String[] { "Normal", "Standard Iron", "Hardcore Iron", "Ultimate Iron", "Group Iron" };
+		String[] text = new String[] { "Normal", "Standard Iron", "Hardcore Iron", "Ultimate Iron", "Group Iron", "Realism" };
 		
-		int[] icon = new int[] { 712, 1142, 711, 1316 };
+		int[] icon = new int[] { 712, 1142, 711, 1316, 1727 };
 		
 		int boxWidth = 127;
+
+		int baseY = 59;
 		
-		configHoverButton(id, "Select Normal", 1040, 1040, 1039, 1039, true, new int[] { id + 1, id + 2, id + 3, id + 4 });
-		rsi.child(child++, id, 20, 89);
+		configHoverButton(id, "Select Normal", 1040, 1040, 1039, 1039, true, new int[] { id + 1, id + 2, id + 3, id + 4, id + 5 });
+		rsi.child(child++, id, 20, baseY + 5);
 		id++;
 		
-		configHoverButton(id, "Select Standard", 1039, 1039, 1040, 1040, false, new int[] { id - 1, id + 1, id + 2, id + 3 });
-		rsi.child(child++, id, 20, 89 + 26);
+		configHoverButton(id, "Select Standard", 1039, 1039, 1040, 1040, false, new int[] { id - 1, id + 1, id + 2, id + 3, id + 4 });
+		rsi.child(child++, id, 20, baseY + 5 + 26);
 		id++;
 		
-		configHoverButton(id, "Select Hardcore", 1039, 1039, 1040, 1040, false, new int[] { id - 2, id - 1, id + 1, id + 2 });
-		rsi.child(child++, id, 20, 89 + 2 * 26);
+		configHoverButton(id, "Select Hardcore", 1039, 1039, 1040, 1040, false, new int[] { id - 2, id - 1, id + 1, id + 2, id + 3 });
+		rsi.child(child++, id, 20, baseY + 5 + 2 * 26);
 		id++;
 		
-		configHoverButton(id, "Select Ultimate", 1039, 1039, 1040, 1040, false, new int[] { id - 3, id - 2, id - 1, id + 1 });
-		rsi.child(child++, id, 20, 89 + 3 * 26);
+		configHoverButton(id, "Select Ultimate", 1039, 1039, 1040, 1040, false, new int[] { id - 3, id - 2, id - 1, id + 1, id + 2 });
+		rsi.child(child++, id, 20, baseY + 5 + 3 * 26);
 		id++;
 		
-		configHoverButton(id, "Select Group", 1039, 1039, 1040, 1040, false, new int[] { id - 4, id - 3, id - 2, id - 1 });
-		rsi.child(child++, id, 20, 89 + 4 * 26);
+		configHoverButton(id, "Select Group", 1039, 1039, 1040, 1040, false, new int[] { id - 4, id - 3, id - 2, id - 1, id + 1 });
+		rsi.child(child++, id, 20, baseY + 5 + 4 * 26);
 		id++;
-		
+
+		configHoverButton(id, "Select Realism", 1039, 1039, 1040, 1040, false, new int[] { id - 5, id - 4, id - 3, id - 2, id - 1 });
+		rsi.child(child++, id, 20, baseY + 5 + 5 * 26);
+		id++;
+
 		for (int i = 0; i < text.length; i++) {
 			if (i != 0) {
 				addSprite(id, icon[i - 1]);
-				rsi.child(child++, id, 50 - 8, 89 + i * 26);
+				rsi.child(child++, id, 50 - (i == text.length - 1 ? 10 : 8), baseY + 5 + i * 26);
 				id++;
 			}
 			
 			addRectangle(id, 160, 0, true, boxWidth - 2, 24 - 2);
-			rsi.child(child++, id, 14, 85 + i * 26);
+			rsi.child(child++, id, 14, baseY + 1 + i * 26);
 			id++;
 			
 			addRectangle(id, 110, 0x4a4333, false, boxWidth, 24);
-			rsi.child(child++, id, 13, 84 + i * 26);
+			rsi.child(child++, id, 13, baseY + i * 26);
 			id++;
 			
 			addText(id, text[i], tda, 1, 0xffb000, false, true);
-			rsi.child(child++, id, i == 0 ? 42 : 56, 89 + i * 26);
+			rsi.child(child++, id, i == 0 ? 42 : 56, baseY + 5 + i * 26);
 			id++;
 		}
-		
+
 		addItemContainer(id, new int[] {16, 6}, new int[] {7, 4}, null, false);
 		rsi.child(child++, id, 160, 64);
 		id++;
@@ -116,14 +122,14 @@ public class StarterWidget extends RSInterface {
 		rsi.child(child++, id, 13, 220);
 		id++;
 		
-		addText(id, "Description:", tda, 3, 0xff9800, false, true);
+		addText(id, "Description:", tda, 2, 0xff9800, false, true);
 		rsi.child(child++, id, 20 + 90 + 9, 224);
 		id++;
 		
 		addHorizontalLine(id, 73, 0xff9800, 100);
 		rsi.child(child++, id, 111 + 9, 224 + 16);
 		id++;
-		
+
 		addText(id, "As a normal player you will be able to play the game \\nwithout any restrictions.", tda, 0, 0xa8a8a8, true, true);
 		rsi.child(child++, id, 151, 224 + 16 + 5);
 		id++;
@@ -135,12 +141,12 @@ public class StarterWidget extends RSInterface {
         rsi.child(child++, id + 1, 209 + 134 + 9, 250 + 29 - 8);
         id += 3;
 
-		addText(id, "Confirm", tda, 3, 0x00ff00, false, true);
-		rsi.child(child++, id, 227 + 134 + 9, 234 + 49 - 8);
+		addText(id, "Confirm", tda, 2, 0x00ff00, false, true);
+		rsi.child(child++, id, 227 + 134 + 9, 234 + 49 - 6);
 		id++;
 		
-		addText(id, "Click '@gre@Confirm'@or1@ to proceed.", tda, 3, 0xffb000, false, true);
-		rsi.child(child++, id, 227 + 134 - 52 + 8, 234 + 49 - 41 - 5);
+		addText(id, "Click '@gre@Confirm'@or1@ to proceed.", tda, 2, 0xffb000, false, true);
+		rsi.child(child++, id, 227 + 134 - 52 + 4, 234 + 49 - 41 - 5);
 		id++;
 	}
 
