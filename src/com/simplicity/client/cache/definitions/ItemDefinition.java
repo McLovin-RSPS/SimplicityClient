cc8f18133f86b99ef78180e4ef37319000ec5411
Blaketon <insane_ownz@hotmail.com>
2021-03-13 18:10:55 +0200

    Support for animating inv sprites

--- src/com/simplicity/client/cache/definitions/ItemDefinition.java
+++ src/com/simplicity/client/cache/definitions/ItemDefinition.java
@@ -1562,7 +1562,7 @@ public final class ItemDefinition {
             sprite.maxWidth = l5;
             sprite.maxHeight = j6;
         }
-        if (k == 0 && i != 5572 && i != 5573 && i != 640 && i != 650 && i != 630) {
+        if (k == 0 && !itemDef.animateInventory && i != 5572 && i != 5573 && i != 640 && i != 650 && i != 630) {
             spriteCache.put(sprite2, i);
         }
         DrawingArea.initDrawingArea(j2, i2, ai1, depthBuffer);
@@ -1714,7 +1714,7 @@ public final class ItemDefinition {
             sprite.maxWidth = l5;
             sprite.maxHeight = j6;
         }
-        if (k == 0 && i != 5572 && i != 5573 && i != 640 && i != 650 && i != 630) {
+        if (k == 0 && !itemDef.animateInventory && i != 5572 && i != 5573 && i != 640 && i != 650 && i != 630) {
             spriteCache.put(sprite2, i);
         }
         DrawingArea.initDrawingArea(j2, i2, ai1, depthBuffer);
@@ -1988,6 +1988,7 @@ public final class ItemDefinition {
         id = -1;
     }
 
+    public boolean animateInventory;
     public byte femaleZOffset;
     public byte femaleXOffset;
     public byte femaleYOffset;
