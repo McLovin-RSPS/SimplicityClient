1b5b7a59e36969b68d61e33edb9cfa19167326f2
Rees <intdubpk@gmail.com>
2021-04-12 16:43:59 +0700

    Demonic Hammers
    Demonic Hammers

--- src/com/simplicity/client/cache/definitions/custom/CustomItems2.java
+++ src/com/simplicity/client/cache/definitions/custom/CustomItems2.java
@@ -1259,7 +1259,7 @@ public class CustomItems2 {
 	            
 	        case 21076:
 	            itemDef.copy(ItemDefinition.forID(4747));
-	            itemDef.name = "Angelic Hammers";
+	            itemDef.name = "Demonic Hammers";
 	            itemDef.editedModelColor = new int[] { 8481, 10291, };
 	            itemDef.newModelColor = new int[] { 908, 908, }; 
 	            //itemDef.newModelColor = new int[] { 127, 127, }; if angelic
