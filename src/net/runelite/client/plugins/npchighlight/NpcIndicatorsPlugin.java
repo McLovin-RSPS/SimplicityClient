e90e3417cb406d848fcd3d5e8d205f2a8f95faf3
Adam <96274526+i-iz-adam@users.noreply.github.com>
2022-01-11 18:51:32 +0000

    Add EXAMINE_NPC to npc indicators menu
    No reason it shouldn't be there..

--- src/net/runelite/client/plugins/npchighlight/NpcIndicatorsPlugin.java
+++ src/net/runelite/client/plugins/npchighlight/NpcIndicatorsPlugin.java
@@ -67,7 +67,7 @@ public class NpcIndicatorsPlugin extends Plugin
 
 	private static final Set<MenuAction> NPC_MENU_ACTIONS = ImmutableSet.of(MenuAction.NPC_FIRST_OPTION, MenuAction.NPC_SECOND_OPTION,
 		MenuAction.NPC_THIRD_OPTION, MenuAction.NPC_FOURTH_OPTION, MenuAction.NPC_FIFTH_OPTION, MenuAction.SPELL_CAST_ON_NPC,
-		MenuAction.ITEM_USE_ON_NPC);
+		MenuAction.ITEM_USE_ON_NPC, MenuAction.EXAMINE_NPC);
 	
 	@Inject
 	private Client client;
