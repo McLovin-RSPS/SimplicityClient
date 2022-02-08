78698a3e5edb6dd1de7a14016a1b80ff3e1279f1
Blaketon <insane_ownz@hotmail.com>
2022-02-08 23:42:29 +0200

    Shooting star hud widget

---
+++ src/com/simplicity/client/widget/ShootingStarHud.java
@@ -0,0 +1,37 @@
+package com.simplicity.client.widget;
+
+/**
+ * A class that represents the Shooting star hud widget.
+ *
+ * @author Blake
+ */
+public class ShootingStarHud extends CustomWidget {
+
+    public static final int WIDGET_ID = 100_000;
+
+    public ShootingStarHud() {
+        super(WIDGET_ID);
+    }
+
+    @Override
+    public void init() {
+        int w = 170;
+        int h = 80;
+
+        int x = 330;
+        int y = 10;
+        add(addRectangle(w, h, 0x373735, 150, true), x, y);
+        add(addRectangle(w, h, 0, 150, false), x, y);
+        add(addText("<spr=1884:-1> Shooting Star <spr=1884:-1>", 0, 0xff9800, true), x + w / 2, y += 10);
+        add(addText("Total Miners: <col=ffffff>0</col>", 0, 0x9d9996, true), x + w / 2, y += 16);
+        add(addText("Total Stardust Mined: <col=ffffff>0</col>", 0, 0x9d9996, true), x + w / 2, y += 16);
+        add(addText("Time left:", 0, 0x9d9996, true), x + w / 2 - 14, y += 16);
+        add(addTimer(w / 2, 16, 0, 0xffffff, "00:00"), x + w / 2 + 28, y);
+    }
+
+    @Override
+    public String getName() {
+        return "Shooting Star";
+    }
+
+}
