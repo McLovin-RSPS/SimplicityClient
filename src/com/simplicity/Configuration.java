049fda9aeef07976ffb417f507af5672da3f4d4f
playerowns <pwned901@gmail.com>
2022-03-18 03:54:42 +0300

    Customs Added

--- src/com/simplicity/Configuration.java
+++ src/com/simplicity/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
 	/**
 	 * Connection information
 	 */
-	public final static boolean LOCALHOST = true;
+	public final static boolean LOCALHOST = false;
 	public final static boolean BETA = true;
 	public static String HOST = LOCALHOST ? "127.0.0.1" : BETA ? "beta.simplicity-data.com" : "149.56.28.66";
 	public static int PORT = 43594;
