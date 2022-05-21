eb093f9e336fc10c057390f871fbc0de287b7c73
Rees <intdubpk@gmail.com>
2022-05-22 01:20:05 +0700

    Fix HD Textures

--- src/com/simplicity/client/Client.java
+++ src/com/simplicity/client/Client.java
@@ -24004,7 +24004,7 @@ public class Client extends RSApplet {
         // animated textures inventory
         
 		for (int i : MOVING_TEXTURES) {
-			if (Rasterizer.anIntArray1480[i] >= j || (i == 61 || i == 62 || i==77 || i == 63  || i == 64  || i == 65 || i == 67 || i == 58) && Rasterizer.anIntArray1480[i] < j) {
+			if (Rasterizer.anIntArray1480[i] >= j || (i == 61 || i == 62 || i == 63  || i == 64  || i == 65 || i == 67 || i == 58) && Rasterizer.anIntArray1480[i] < j) {
 				Background texture = Rasterizer.textures[i];
 				
 				try {
